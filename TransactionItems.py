719ba00b8286ae35ef29f48063034226e846541d
DhruvVishwanath16 <119365850+DhruvVishwanath16@users.noreply.github.com>
2022-11-30 15:34:52 -0600

    Update TransactionItems.py

--- TransactionItems.py
+++ TransactionItems.py
@@ -1,17 +1,47 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 28 16:08:18 2022
-
-@author: echoe
-"""
-
-class TransactionItemClass:
+class TransactionItem:
     
     
-    def __init__(self, new_id, new_name, new_stock, new_price):
+    def __init__(self):
+        self.__id = 0
+        self.__name = ''
+        self.__qty = 0
+        self.__price = 0
+    
+    def get_id(self):
+        return self.__id
+    
+    def set_id(self, new_id):
         self.__id = new_id
+    
+    
+    def get_name(self):
+        return self.__name
+    
+    def set_name(self, new_name):
         self.__name = new_name
-        self.__quantity = new_stock
+        
+    
+    def get_qty(self):
+        return self.__qty
+    
+    def set_qty(self, new_qty):
+        if new_qty>0:
+            self.__qty = new_qty
+            
+    
+    def get_price(self):
+        return self.__price
+    
+    def set_price(self, new_price):
         self.__price = new_price
         
+    
+    def calc_cost(self):
+        cost = self.__qty*self.__cost
+        return cost
+    
+    def __str__(self):
+        
+        
+    
     
